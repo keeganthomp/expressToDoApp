79f73f205240c0515ccfc68080bb842ccbb985d2
keeganthomp <whereiskeegan@gmail.com>
2017-06-21 16:40:37 -0500

    Made basic styling and added functionality to pull in entered todo's

---
+++ app2.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const mustacheExpress = require("mustache-express");
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+const port = 7500;
+
+app.engine("mustache", mustacheExpress());
+app.set("views", "./views");
+app.set("view engine", "mustache");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
+
+app.use("/", express.static("./views"));
+app.get("/", function(req, res) {
+  res.render("index");
+});
+var toDoArray = [];
+app.post("/", function(req, res) {
+  var todo = req.body;
+  toDoArray.push(todo);
+  res.render("index", { todo: toDoArray });
+  // console.log(todo);
+});
+
+app.listen(port, function(req, res) {
+  console.log("You are up and runnning on", port);
+});
